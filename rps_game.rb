eb355ac3bf86cb010c2b02d511e31db4e42dcfd0
Seiya Wada <seiya.sg@gmail.com>
2012-08-29 18:31:10 +0900

    Add conditions Pass test_simple_win_lose

--- rps_game.rb
+++ rps_game.rb
@@ -1,6 +1,6 @@
 def rps_game(players)
   rockPlayers = []; scissorsPlayers = []; paperPlayers = [];
-
+  turn = 0
   players.size.times do |i|
     hand = players[i].shift
     if hand == :rock
@@ -12,7 +12,13 @@ def rps_game(players)
     end
   end
   
-  if rockPlayers.size == 0 && scissorsPlayers.size == 0 && paperPlayers.size == 0
+  if (rockPlayers.size == 0 && scissorsPlayers.size == 0 && paperPlayers.size == 0) #draw end
     return [-1, -1]
+  elsif (rockPlayers.size == 1 && paperPlayers.size == 0) #rock wins scissors
+    return [turn,rockPlayers[0]]
+  elsif (scissorsPlayers.size == 1 && rockPlayers.size == 0)  #scissors wins paper
+    return [turn,scissorsPlayers[0]]
+  elsif (paperPlayers.size == 1 && scissorsPlayers.size == 0) #paper win rock
+    return [turn,paperPlayers[0]]
   end
 end
