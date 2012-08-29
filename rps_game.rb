ccd3e4a34b7545ea5ad21d9da4c27e2aaebb825b
Seiya Wada <seiya.sg@gmail.com>
2012-08-29 17:53:51 +0900

    Add a conditional Pass test_no_player

--- rps_game.rb
+++ rps_game.rb
@@ -1,2 +1,18 @@
 def rps_game(players)
+  rockPlayers = []; scissorsPlayers = []; paperPlayers = [];
+
+  players.size.times do |i|
+    hand = players[i].shift
+    if hand == :rock
+      rockPlayers.push i
+    elsif hand == :scissors
+      scissorsPlayers.push i
+    elsif hand == :paper
+      paperPlayers.push i
+    end
+  end
+  
+  if rockPlayers.size == 0 && scissorsPlayers.size == 0 && paperPlayers.size == 0
+    return [-1, -1]
+  end
 end
