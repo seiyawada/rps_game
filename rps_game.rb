b70a6bf2b510971c2d02c17c62e78713761285d1
Seiya Wada <seiya.sg@gmail.com>
2012-09-24 01:59:26 +0900

    Pass test_complex_win_lose

--- rps_game.rb
+++ rps_game.rb
@@ -19,7 +19,22 @@ def rps_game(players, turn = 0)
   elsif (scissorsPlayers.size == 1 && rockPlayers.size == 0)  #scissors wins paper
     return [turn,scissorsPlayers[0]]
   elsif (paperPlayers.size == 1 && scissorsPlayers.size == 0) #paper win rock
-    return [turn,paperPlayers[0]]
+    return [turn,paperPlayers[0]]  
+  elsif (paperPlayers.size == 0) #rock wins scissors, next game
+    scissorsPlayers.each do |i|
+      players[i] = []
+    end
+    return rps_game(players, turn = turn + 1)
+  elsif (rockPlayers.size == 0)  #scissors wins paper, next game
+    paperPlayers.each do |i|
+      players[i] = []
+    end
+    return rps_game(players, turn = turn + 1)
+  elsif (scissorsPlayers.size == 0) #paper win rock, next game
+    rockPlayers.each do |i|
+      players[i] = []
+    end
+    return rps_game(players, turn = turn + 1)
   else 
     return rps_game(players, turn = turn + 1) #draw, next game
   end
